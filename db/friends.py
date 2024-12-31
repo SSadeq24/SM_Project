f86fc78505fc60de03705690accf33b01cd1fab3
SerhiyPi <serhiy.piven@motopp.nl>
2024-12-31 15:50:06 +0100

    added files

---
+++ db/friends.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+from db import db_user
+from db.database import get_db
+from db.models import DbUser
+from schemas import Friend_Request, User
+
+router = APIRouter(
+    prefix="/friends",
+    tags=["friends"]
+)
+
+@router.get('/{id}')
+def get_friends(id: int, db: Session = Depends(get_db)):
+    return db_user.get_friends(id, db)
+
+
+@router.put("/{id}/send")
+def send_friend_request(id: int, request: User, db: Session = Depends(get_db)):
+    return db_user.send_request(id, request, db) 
+
+
+@router.put("/{id}/aprove")
+def approve_friend_request(id: int, request: Friend_Request, db: Session = Depends(get_db)):
+    return db_user.approve_request(id, request, db) 
+
+
+@router.put("/{id}/deny")
+def dreny_friend_request(id: int, request: Friend_Request, db: Session = Depends(get_db)):
+    return db_user.deny_request(id, request, db) 
--- db/models.py
+++ db/models.py
@@ -21,3 +21,20 @@ class DbPost(Base):
   user_id = Column(Integer, ForeignKey('user.id'))
   user = relationship('DbUser', back_populates='items')
 
+class DbFriendship(Base):
+    __tablename__ = "friendship"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    # friend_id = Column(Integer, ForeignKey('users.id'))
+    # friendships = relationship("DbFriendship", foreign_keys="[DbFriendship.user_id]")
+    friend_id = Column(Integer) 
+   
+class DbFriendships(Base):
+     __tablename__ = "friendships"
+     id = Column(Integer, primary_key=True, index=True)
+     user_id = Column(Integer, ForeignKey('users.id'))
+    # friend_id = Column(Integer, ForeignKey('users.id'))
+    # friendships = relationship("DbFriendship", foreign_keys="[DbFriendship.user_id]")
+     friends_id = Column(Integer) 
+     friends_request = Column(Integer)
+
