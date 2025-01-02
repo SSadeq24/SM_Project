fa86b824add22effbc2d80f5259e3834acab43a1
SerhiyPi <serhiy.piven@motopp.nl>
2025-01-02 10:58:19 +0100

    add friends router

--- Together.py
+++ Together.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from db import models
-from db.database import engine
-from routers import user
+from db.database import engine, Base
+from routers import user, friends, post
 from routers import post
 from Auth import authentication
+from db.models import Base 
 
 
 app = FastAPI()
@@ -11,9 +12,13 @@ app = FastAPI()
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(authentication.router)
+app.include_router(friends.router)
 
 @app.get('/')
 def root():
     return {"message": "Welcome to LinkUp, a social network project by Samar, Sadeq, Serhiy, and Ali"}
 
 models.Base.metadata.create_all(engine)
+
+Base.metadata.create_all(engine)
+
