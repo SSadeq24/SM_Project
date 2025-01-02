4b5821e3984a3b991e66a4191e7008b0f23dd1aa
SSadeq24 <sadeq.sadeq@motopp.nl>
2025-01-02 18:28:39 +0100

    Update Together.py

--- Together.py
+++ Together.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from db import models
-from db.database import engine, Base
-from routers import user, friends, post
+from db.database import engine
+from routers import user
 from routers import post
 from Auth import authentication
-from db.models import Base 
 
 
 app = FastAPI()
@@ -12,13 +11,9 @@ app = FastAPI()
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(authentication.router)
-app.include_router(friends.router)
 
 @app.get('/')
 def root():
-    return {"message": "Welcome to LinkUp, a social network project by Samar, Sadeq, Serhiy, and Ali"}
+    return "Hello World!"
 
 models.Base.metadata.create_all(engine)
-
-Base.metadata.create_all(engine)
-
